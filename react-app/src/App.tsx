92f04263ceb058a761f47e87c01de3bbd179658a
TewodrosAdimas <yosefenadagim@gmail.com>
2024-11-29 12:35:56 +0300

    fetch

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-interface User{
-  id:number;
+interface User {
+  id: number;
   name: string;
 }
 
 const App: React.FC = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => console.log(res.data[0].name));
-  });
-  return <div></div>;
+      .then((res) => setUsers(res.data));
+  }, []);
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default App;
